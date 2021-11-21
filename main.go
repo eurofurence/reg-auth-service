b2b356cfb14a7d563c8dac249b2ca4126ea0ecc0
Fenrikur <ef-registration@fenrikur.de>
2021-11-21 15:28:39 +0100

    open and close database in main

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/eurofurence/reg-auth-service/internal/repository/config"
+	"github.com/eurofurence/reg-auth-service/internal/repository/database"
 	"github.com/eurofurence/reg-auth-service/internal/repository/logging/consolelogging/logformat"
 	"github.com/eurofurence/reg-auth-service/web"
 )
@@ -14,6 +15,10 @@ func main() {
 	if err != nil {
 		log.Fatal(logformat.Logformat("ERROR", "00000000", fmt.Sprintf("Error while loading configuration: %v", err)))
 	}
+
+	database.Open()
+	defer database.Close()
+
 	server := web.Create()
 	web.Serve(server)
 }
